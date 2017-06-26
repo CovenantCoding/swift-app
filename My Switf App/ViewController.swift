5c03af2393163cd22223379e0a2be948375aa6cc
Michael Gray <MichaelJGray@outlook.com>
2017-06-26 10:28:52 -0400

    My first commit!!!! Yayay Mr. Gray you're doing it!

--- My Switf App/ViewController.swift
+++ My Switf App/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myCoolLabel: UILabel!
+    
+    
+    @IBAction func buttonPushed(_ sender: Any) {
+        
+        myCoolLabel.text = "Good Morning Mr. Gray"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
